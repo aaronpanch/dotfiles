e2fe338da0ba4915aad5785a1256098d47260df5
Aaron Panchal <aaron.panchal@gmail.com>
2018-06-18 07:13:36 -0700

    Add .zshrc and oh my zsh theme

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ GLOBIGNORE=".:..:.git:.DS_Store" # Skip non-project dotfiles
 dir="$(cd "$(dirname "$0")"; pwd)"
 
 for x in .*; do
-  rm ~/$x # remove the old file
+  rm -r ~/$x # remove the old file
   ln -s $dir/$x ~/$x # create symlink
 done
